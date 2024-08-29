e9f8af416a7429a4aac2df6d750a05a4497ae9c7
Erik Kovach <kovacherik@gmail.com>
2024-08-29 14:46:37 -0300

    feat: Evaluation and testing of the model.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
 
 nltk.download("movie_reviews") 
 
@@ -23,8 +24,10 @@ for review in reviews:
 
 df = pd.DataFrame(reviews, columns=["text", "sentiment"])
 
+vect = CountVectorizer(max_features=2000)
+
 # Transforming the text data into vectors, so that it can be used by the model.
-review_tokens = CountVectorizer(max_features=2000).fit_transform(df["text"])
+review_tokens = vect.fit_transform(df["text"])
 sentiments = df["sentiment"]
 
 STATE = 50 # Arbitrary integer. Guarantees the dataset is consistently split in the same way every time.
@@ -37,3 +40,17 @@ reviews_train, reviews_test, sentiment_train, sentiment_test = train_test_split(
 # Naive Bayes classifier 
 model = MultinomialNB()
 model.fit(reviews_train, sentiment_train) # Trains the model
+
+sentiment_prediction = model.predict(reviews_test) # Test the model with the test data
+
+print(f"-Accuracy: {accuracy_score(sentiment_test, sentiment_prediction)}")
+print(f"-Classification Report:\n{classification_report(sentiment_test, sentiment_prediction)}")
+
+def predict_sentiment(text):
+    return model.predict(vect.transform([text]))[0]
+
+
+# Test
+print(predict_sentiment("Fantastic movie!"))
+print(predict_sentiment("It was a terrible. Dislike."))
+print(predict_sentiment("The movie was okay, nothing special."))
