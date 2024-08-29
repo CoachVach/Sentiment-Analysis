c8c8ab76ef39a2c8098a894b45cfcfbd30d6a7ef
Erik Kovach <kovacherik@gmail.com>
2024-08-29 14:12:42 -0300

    feat: Data to df. Data splitted into Train and Test.

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, classification_report
-
 
 nltk.download("movie_reviews") 
 
@@ -18,5 +15,20 @@ reviews = [
     for fileid in movie_reviews.fileids(category)
 ]
 
+"""
 for review in reviews: 
     print(review)
+"""
+
+df = pd.DataFrame(reviews, columns=["text", "sentiment"])
+
+# Transforming the text data into vectors, so that it can be used by the model.
+review_tokens = CountVectorizer(max_features=2000).fit_transform(df["text"])
+sentiments = df["sentiment"]
+
+STATE = 50 # Arbitrary integer. Guarantees the dataset is consistently split in the same way every time.
+
+# Splits the data into training and testing sets (20% for testing).
+reviews_train, reviews_test, sentiment_train, sentiment_test = train_test_split(
+    review_tokens, sentiments, test_size=0.2, random_state=STATE
+)
